4508097f25769b20bf120b49097d1308d61536d9
Sönke Hahn <soenkehahn@gmail.com>
2020-10-11 21:36:19 -0400

    make a list

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,18 @@
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<Model>::new().mount_to_body();
+}
+
 struct Model {
     link: ComponentLink<Self>,
-    value: String,
+    value: Vec<i32>,
 }
 
 enum Msg {
-    AddOne,
+    Modify(fn(&mut Model)),
 }
 
 impl Component for Model {
@@ -17,73 +22,39 @@ impl Component for Model {
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             link,
-            value: "".to_string(),
+            value: vec![],
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.value = format!("{}a", self.value),
+            Msg::Modify(f) => f(self),
         }
         true
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
         false
     }
 
     fn view(&self) -> Html {
+        let list = self
+            .value
+            .iter()
+            .map(|item: &i32| -> Html {
+                html! {
+                    <li> {item} </li>
+                }
+            })
+            .collect::<Html>();
+
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ &self.value }</p>
-                <Foo count={self.value.len()} />
+                <button onclick=self.link.callback(|_| Msg::Modify(|model| model.value.push(model.value.len() as i32)))>{ "click me" }</button>
+                <ul>
+                    { list }
+                </ul>
             </div>
-        }
-    }
-}
-
-#[wasm_bindgen(start)]
-pub fn run_app() {
-    App::<Model>::new().mount_to_body();
-}
-
-#[derive(Clone, Properties)]
-struct FooProps {
-    count: usize,
-}
-
-struct Foo {
-    len: usize,
-}
-
-struct Never {
-    _never: &'static Never,
-}
-
-impl Component for Foo {
-    type Message = Never;
-    type Properties = FooProps;
-
-    fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Foo { len: props.count }
-    }
-
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        false
-    }
-
-    fn change(&mut self, props: Self::Properties) -> ShouldRender {
-        self.len = props.count;
-        true
-    }
-
-    fn view(&self) -> Html {
-        html! {
-            {format!("this is Foo: {}", self.len)}
         }
     }
 }
